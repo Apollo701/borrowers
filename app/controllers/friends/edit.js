e6b3b0f401c6ef863a9c01d038180d1026abe9bf
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-07 16:15:56 -0700

    pre formatting

---
+++ app/controllers/friends/edit.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    cancel: function() {
+      this.transitionToRoute('friends.show', this.get('model'));
+      
+      return false;
+    }
+  }
+});
